ad2aedc2e1c63077ab19edb214f9d5e04aaa89d4
Reventless <stephan.lamersdorf@gmail.com>
2021-07-15 18:36:25 +0200

    V2 works

--- Boxv2.php
+++ Boxv2.php
@@ -18,14 +18,15 @@ $block = [
 function mine($block){
     $hashedblock = "hello";
     $hashedminedblock = "test";
-    $stringdump = "";
-    $stringdump = $block['number'].$block['data'].$block['nonce'];
-    while ($hashedblock[0] != 1 && $hashedblock[1] != 3 && $hashedblock[2] != 3 && $hashedblock[3] != 7 ){
+    while ($hashedblock[0] != "1" || $hashedblock[1] != "3" || $hashedblock[2] != "3" || $hashedblock[3] != "7" ){
         $block['nonce'] = rand(0, 9999999);
+        $stringdump = $block['number'].$block['data'].$block['nonce'];
         $hashedblock = hash("sha256", $stringdump);
         var_dump($hashedblock);
     }
     echo "MINED!";
     echo $block['nonce'];
+    echo "\n";
+    echo $hashedblock;
 }
 mine($block);
